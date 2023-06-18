60650091fc4af70ee0bfa32dd4e58df7a70fa453
Shamanth <shamanth1604@gmail.com>
2023-06-18 13:49:26 +0530

    Restructure env vars

--- apps/backend/src/modules/queue.ts
+++ apps/backend/src/modules/queue.ts
@@ -1,11 +1,14 @@
 import { Queue, Worker } from "bullmq";
 import ModGenerate from "./generate";
+import { Redis } from "ioredis";
 
 const QUEUE_NAME = "default";
-if (!process.env.REDIS_HOST) console.warn("REDIS_HOST is not defined");
-const connection = {
-    host: process.env.REDIS_HOST,
-};
+
+if (!process.env.REDIS_URL) console.warn("REDIS_URL is not defined");
+const connection = new Redis(process.env.REDIS_URL!);
+// const connection = {
+//     host: process.env.REDIS_HOST,
+// };
 
 const queue = new Queue(QUEUE_NAME, { connection });
 
